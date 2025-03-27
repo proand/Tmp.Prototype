10fba55156025efee7eabd0af12f3066656bde1a
Bernt Andersen <bernt.andersen@fhi.no>
2025-03-27 16:25:01 +0100

    Add search input and sokeresultat

--- src/app/GISBAS-domain/domain-components/standars-sok/standars-sok.component.ts
+++ src/app/GISBAS-domain/domain-components/standars-sok/standars-sok.component.ts
@@ -4,6 +4,16 @@ import { SpinnerComponent } from '@app/shared/components/spinner/spinner.compone
 import { NavigationService } from '@app/shared/navigation.service';
 import { GisbasViewStateService } from '@GISBAS_CONNECT/gisbas-view-state.service';
 
+interface Sokeresultat {
+  Navn: string;
+  TypeInfokort: string;
+  Gruppetype: string;
+  Synonym: string;
+  Kategori: string;
+  Status: string;
+  InfokortLink: string;
+}
+
 @Component({
   selector: 'app-standars-sok',
   imports: [SharedModule, SpinnerComponent],
@@ -15,6 +25,8 @@ export class StandarsSokComponent {
   private navigationService = inject(NavigationService);
 
   showStandarsSok = false;
+  soketerm = '';
+  sokeresultat: Sokeresultat[] | null = null;
 
   constructor() {
     setTimeout(() => {
@@ -29,7 +41,70 @@ export class StandarsSokComponent {
     }, 1000);
   }
 
+  sok(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.sokeresultat = this.getSokeresultat(this.soketerm);
+    } else {
+      this.sokeresultat = null;
+    }
+  }
+
   navigate(routerLink: string) {
     this.navigationService.navigate(routerLink);
+  }
+
+  getSokeresultat(soketerm: string): Sokeresultat[] | null {
+    soketerm = soketerm.slice(0, 4);
+
+    console.log('soketerm', soketerm);
+
+    switch (soketerm) {
+      case 'para':
+        return [
+          {
+            Navn: 'Paracet',
+            TypeInfokort: 'Produkt',
+            Gruppetype: 'Analgetica, paracetamolholdige',
+            Synonym: '',
+            Kategori: 'Legemiddel',
+            Status: 'Godkjent',
+            InfokortLink: '0/2',
+          },
+          {
+            Navn: 'Paracetamol',
+            TypeInfokort: 'Kjemisk forbindelse',
+            Gruppetype: '',
+            Synonym: 'Acetaminophen, N-acetyl-aminofenol',
+            Kategori: 'Kjemisk forbindelse',
+            Status: 'Godkjent',
+            InfokortLink: 'TODO',
+          },
+        ];
+
+      case 'ibux':
+        return [
+          {
+            Navn: 'Ibux',
+            TypeInfokort: 'Produkt',
+            Gruppetype: 'NSAIDS',
+            Synonym: '',
+            Kategori: 'Legemiddel',
+            Status: 'Godkjent',
+            InfokortLink: 'TODO',
+          },
+          {
+            Navn: 'Ibux gel',
+            TypeInfokort: 'Legemiddel',
+            Gruppetype: 'Analgetika yil utvortes bruk, NSAIDS',
+            Synonym: '',
+            Kategori: 'Legemiddel',
+            Status: 'Godkjent',
+            InfokortLink: 'TODO',
+          },
+        ];
+
+      default:
+        return [];
+    }
   }
 }
